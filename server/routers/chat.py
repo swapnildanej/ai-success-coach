6cb46826f5d945d1f34503ea0176c2674ed2486d
swapnildanej <swapnildanej@gmail.com>
2025-09-22 17:08:05 +0530

    Update chat.py

--- server/routers/chat.py
+++ server/routers/chat.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
-from models.pydantic import ChatRequest, ChatResponse
-from services.openai_client import get_chat_completion
-from utils.auth import verify_bearer
+from ..models.schema import ChatRequest, ChatResponse
+from ..services.openai_client import get_chat_completion
+from ..utils.auth import verify_bearer
 
 router = APIRouter(dependencies=[Depends(verify_bearer)])
 
